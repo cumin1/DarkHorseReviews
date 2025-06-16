08e56f3797ab7afc85426e685719f9aebff48eed
cumin1 <3144941746@qq.com>
2025-06-16 21:13:05 +0800

    使用lua脚本实现释放锁操作的原子性

--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,7 +1,11 @@
 package com.hmdp.utils;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
+import java.util.Collections;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -10,6 +14,11 @@ public class SimpleRedisLock implements ILock{
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString()+"-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static{
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+    }
 
     private String name;
     private StringRedisTemplate stringRedisTemplate;
@@ -29,14 +38,21 @@ public class SimpleRedisLock implements ILock{
         return Boolean.TRUE.equals(success);
     }
 
-    @Override
-    public void unlock() {
-        // 获取并判断线程标识
-        String threadId =  ID_PREFIX + Thread.currentThread().getId();
-        String redisId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if(threadId.equals(redisId)) {
-            // 如果线程标识和锁里的标识一样 则释放锁
-            stringRedisTemplate.delete(KEY_PREFIX+name);
-        }
+//    @Override
+//    public void unlock() {
+//        // 获取并判断线程标识
+//        String threadId =  ID_PREFIX + Thread.currentThread().getId();
+//        String redisId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        if(threadId.equals(redisId)) {
+//            // 如果线程标识和锁里的标识一样 则释放锁
+//            stringRedisTemplate.delete(KEY_PREFIX+name);
+//        }
+//    }
+
+    public void unlock(){
+        // 使用lua脚本实现释放锁操作的原子性
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
 }
